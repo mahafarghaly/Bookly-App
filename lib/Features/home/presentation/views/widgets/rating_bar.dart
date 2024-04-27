1b2ea8848afb9cf8245b92bcfe2a94da4da9dbed
Maha <mahafarghalymo@gmail.com>
2024-04-27 01:09:13 -0700

    create books action

--- lib/Features/home/presentation/views/widgets/rating_bar.dart
+++ lib/Features/home/presentation/views/widgets/rating_bar.dart
@@ -4,18 +4,27 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../../../../../core/utils/styles.dart';
 
 class BookingRate extends StatelessWidget {
-  const BookingRate({super.key});
 
+  const BookingRate({super.key,  this.mainAxisAlignment=MainAxisAlignment.start});
+  final MainAxisAlignment mainAxisAlignment;
   @override
   Widget build(BuildContext context) {
-    return const Row(
-      children: [
-        Icon(FontAwesomeIcons.solidStar,
+    return  Row(
+      mainAxisAlignment:mainAxisAlignment,
+      children:  [
+        const Icon(FontAwesomeIcons.solidStar,
+          weight: 14,
           color: Color(0XffFFDD4F),),
-        SizedBox(width: 6.3),
-        Text("4.5",style: Styles.textStyle16,),
-        SizedBox(width: 5),
-        Text('(245)',style: Styles.textStyle14,)
+        const SizedBox(width: 6.3),
+        const Text("4.5",style: Styles.textStyle16,),
+        const SizedBox(width: 5),
+        Opacity(
+            opacity: 0.5,
+            child: Text('(245)',style: Styles.textStyle14.copyWith(
+              fontWeight: FontWeight.w600
+            ),
+            ),
+        )
 
       ],
     );
