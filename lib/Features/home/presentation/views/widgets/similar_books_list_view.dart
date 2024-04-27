bc5380446eee0b09279e0e1a01411fc82ce26568
Maha <mahafarghalymo@gmail.com>
2024-04-27 01:49:21 -0700

    add similar books list view

--- lib/Features/home/presentation/views/widgets/similar_books_list_view.dart
+++ lib/Features/home/presentation/views/widgets/similar_books_list_view.dart
@@ -2,19 +2,19 @@ import 'package:flutter/material.dart';
 
 import 'custom_book_item.dart';
 
-class FeaturedBooksListView  extends StatelessWidget {
-  const FeaturedBooksListView ({super.key});
+class SimilarBooksListView extends StatelessWidget {
+  const SimilarBooksListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  SizedBox(
-      height: MediaQuery.of(context).size.height*0.3,
+    return SizedBox(
+      height: MediaQuery.of(context).size.height*0.15,
       child: ListView.builder(
           scrollDirection: Axis.horizontal,
           itemCount: 10,
           itemBuilder: (context,index){
             return const Padding(
-              padding:  EdgeInsets.symmetric(horizontal: 8.0),
+              padding:  EdgeInsets.symmetric(horizontal: 5.0),
               child:  CustomBookImage(),
             );
           }),
