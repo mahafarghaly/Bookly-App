839a3308b6dd67c5eb21650be96bda006e489d9d
Maha <mahafarghalymo@gmail.com>
2024-04-29 23:58:52 -0700

    creat api service class

---
+++ lib/Features/home/data/models/repos/home_repo.dart
@@ -0,0 +1,9 @@
+import 'package:book_app/Features/home/data/models/book_model.dart';
+import 'package:book_app/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo{
+  Future<Either<Failure,List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure,List<BookModel>>> fetchFeatureBooks();
+
+}
